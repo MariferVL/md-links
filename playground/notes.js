cd8621e2c676bc3bebc9bf52599b72fff5853add
Marifer Villalobos <mfvl.1186@gmail.com>
2023-04-22 17:31:40 -0400

    crear zona de creación de código

---
+++ playground/notes.js
@@ -0,0 +1,10 @@
+  // instructs Node.js to terminate the process synchronously with an exit status of code
+  process.exit()
+  // returns an object containing the user environment. 
+    process.env
+// get absolute route till the file in cjs
+__filename 
+//get absolute route till the dir cjs
+  __dirname
+  // get absolute route till the file in mjs
+  import.meta.url
