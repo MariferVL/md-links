117d9c99efd847ff68be0a6569b5130711ee0808
Marifer Villalobos <mfvl.1186@gmail.com>
2023-04-22 17:31:15 -0400

    Primer intento de código

--- index.js
+++ index.js
@@ -1,30 +1,78 @@
-module.exports = () => {
+//TODO: index.js: Desde este archivo debes exportar una función (mdLinks).
+import fs from 'fs';
+import path from 'path';
+import chalk from 'chalk';
+import fetch from 'node-fetch';
 
-  const mdLinks = require("md-links");
-
-  mdLinks("./some/example.md")
-    .then(links => {
-      // => [{ href, text, file }, ...]
-    })
-    .catch(console.error);
-
-  mdLinks("./some/example.md", { validate: true })
-    .then(links => {
-      // => [{ href, text, file, status, ok }, ...]
-    })
-    .catch(console.error);
-
-  mdLinks("./some/dir")
-    .then(links => {
-      // => [{ href, text, file }, ...]
-    })
-    .catch(console.error);
-
-  const input = 'md-links ./some/example.md --validate';
-  const path = input.replace(/^md-links\s+/, '');
-  console.log(path); // './some/example.md --validate'
-
+const mdLinks = (filePath, options = {}) => {
+  const absolutePath = path.resolve(filePath);
+  const fileStats = fs.statSync(absolutePath);
+  if (fileStats.isDirectory()) {
+    const fileNames = fs.readdirSync(absolutePath);
+    const promises = fileNames.map((fileName) => mdLinks(path.join(absolutePath, fileName), options));
+    return Promise.all(promises).then((links) => [].concat(...links));
+  }
+  if (!fileStats.isFile() || path.extname(absolutePath) !== '.md') {
+    return Promise.reject(new Error('Invalid file'));
+  }
+  return new Promise((resolve) => {
+    fs.readFile(absolutePath, 'utf-8', (err, fileContent) => {
+      const links = [];
+      const regex = /\[([^\]]+)\]\((http[s]?:\/\/[^\s^)]+)\)/gm;
+      let match = regex.exec(fileContent);
+      while (match != null) {
+        const [fullMatch, text, href] = match;
+        links.push({ href, text, file: absolutePath });
+        match = regex.exec(fileContent);
+      }
+      if (options.validate) {
+        const linkPromises = links.map((link) => {
+          return fetch(link.href).then((res) => {
+            link.status = res.status;
+            link.ok = res.status >= 200 && res.status < 400 ? 'ok' : 'fail';
+            return link;
+          }).catch((err) => {
+            link.status = null;
+            link.ok = 'fail';
+            return link;
+          });
+        });
+        return Promise.all(linkPromises).then(resolve);
+      }
+      return resolve(links);
+    });
+  });
 };
 
+const printLinks = (links) => {
+  links.forEach((link) => {
+    console.log(`${chalk.blue(link.file)} ${chalk.yellow(link.href)} ${chalk.green(link.text.substring(0, 50))}`);
+  });
+};
+ 
+const printStats = (links) => {
+  const total = links.length;
+  const unique = [...new Set(links.map((link) => link.href))].length;
+  const broken = links.filter((link) => link.ok === 'fail').length;
+  console.log(`Total: ${total}\nUnique: ${unique}${options.validate ? `\nBroken: ${broken}` : ''}`);
+};
 
-//TODO: index.js: Desde este archivo debes exportar una función (mdLinks).
+if (require.main === module) {
+  const args = process.argv.slice(2);
+  const filePath = args[0];
+  const options = {};
+  if (args.includes('--validate')) {
+    options.validate = true;
+  }
+  mdLinks(filePath, options)
+    .then((links) => {
+      if (args.includes('--stats')) {
+        printStats(links);
+      } else {
+        printLinks(links);
+      }
+    })
+    .catch((err) => console.error(err.message));
+}
+
+export { mdLinks };
