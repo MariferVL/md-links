0ca4ec3f600f8a0613019ceef5184d2a35980925
Marifer Villalobos <mfvl.1186@gmail.com>
2023-04-22 20:44:02 -0400

    corregir log de Error

--- Hitos/leerArchivoEstilo.js
+++ Hitos/leerArchivoEstilo.js
@@ -9,12 +9,12 @@ import chalk from 'chalk';
 export function mdLinks() {
   const filePath = process.argv[2];
   if (!filePath) {
-    console.error(chalk.white.bgRed.bold('Error '),chalk.red('Debes ingresar la ruta del archivo a leer'));
+    console.error(chalk.white.bgRed.bold('Error: '),chalk.red('Debes ingresar la ruta del archivo a leer'));
     return;
   }
   fs.readFile(filePath, 'utf-8', (err, data) => {
     if (err) {
-      console.error(chalk.whiteBright.bgRed.bold('Error '), chalk.red(`al leer el archivo: ${err}`));
+      console.error(chalk.whiteBright.bgRed.bold('Error: '), chalk.red(`al leer el archivo: ${err}`));
     } else {
       const regex = /\[(?<text>.*?)\]\((?<url>.*?)\)/g;
       let match;
