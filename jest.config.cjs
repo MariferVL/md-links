961ab130d1d196b5f8f7a347fd31b2a9150d1d7b
Marifer Villalobos <mfvl.1186@gmail.com>
2023-04-24 01:02:07 -0400

    configurar archivos para pruebas unitarias

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    transform: {},
+  };
+
+
+  //export default {
+    //   "transform": {
+    //     "^.+\\.jsx?$": "babel-jest"
+    //   }
+    // }
+    
+  //jest.config.js
