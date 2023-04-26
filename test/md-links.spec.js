590791305140866bcb6486ca7927b8cf132ed9ea
Marifer Villalobos <mfvl.1186@gmail.com>
2023-04-26 00:45:10 -0400

    prueba unitaria hito 1 funcionando 93% cobertura, 100% ok

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -2,7 +2,7 @@
 //Tu inplementación debe pasar estos tets.
 import { mdLinks } from '../md-links.js';
 import fs from 'fs';
-
+import chalk from 'chalk';
 
 describe('mdLinks', () => {
   test('should print an error message when no file path is provided', () => {
@@ -11,7 +11,7 @@ describe('mdLinks', () => {
 
     mdLinks();
 
-    expect(console.error).toHaveBeenCalledWith('Error: ', 'Debes ingresar la ruta del archivo a leer');
+    expect(console.error).toHaveBeenCalledWith(chalk.white.bgRed.bold('Error: '),chalk.red('Debes ingresar la ruta del archivo a leer'));
   });
 
   test('should print the href, text, and file name of the links in the file', () => {
@@ -26,15 +26,15 @@ describe('mdLinks', () => {
 
     mdLinks();
 
-    expect(console.log).toHaveBeenCalledWith('\n\n\t\t\t\t Md Links ');
-    expect(console.log).toHaveBeenCalledWith(`\nEl contenido de ${filePath} es: \n`);
-    expect(console.log).toHaveBeenCalledWith('href: ', 'https://www.google.com');
-    expect(console.log).toHaveBeenCalledWith('text: ', 'Google');
-    expect(console.log).toHaveBeenCalledWith('file: ', filePath);
+    expect(console.log).toHaveBeenCalledWith(chalk.black.bgHex('#00F5FF').bold.underline('\n\n\t\t\t\t Md Links '));
+    expect(console.log).toHaveBeenCalledWith(chalk.black.bgHex('#00F5FF').bold(`\nEl contenido de ${filePath} es: \n`));
+    expect(console.log).toHaveBeenCalledWith(chalk.whiteBright.bgHex('#EA047E').bold('href: '), chalk.hex('#EA047E')('https://www.google.com'));
+    expect(console.log).toHaveBeenCalledWith(chalk.bgHex('#FF6D28').bold('text: '), chalk.hex('#FF6D28')('Google'));
+    expect(console.log).toHaveBeenCalledWith(chalk.bgHex('#FCE700').bold('file: '), chalk.hex('#FCE700')(filePath));
     expect(console.log).toHaveBeenCalledWith('');
-    expect(console.log).toHaveBeenCalledWith('href: ', 'https://www.facebook.com');
-    expect(console.log).toHaveBeenCalledWith('text: ', 'Facebook');
-    expect(console.log).toHaveBeenCalledWith('file: ', filePath);
+    expect(console.log).toHaveBeenCalledWith(chalk.whiteBright.bgHex('#EA047E').bold('href: '), chalk.hex('#EA047E')('https://www.facebook.com'));
+    expect(console.log).toHaveBeenCalledWith(chalk.bgHex('#FF6D28').bold('text: '), chalk.hex('#FF6D28')('Facebook'));
+    expect(console.log).toHaveBeenCalledWith(chalk.bgHex('#FCE700').bold('file: '), chalk.hex('#FCE700')(filePath));
     expect(console.log).toHaveBeenCalledWith('');
   });
 });
